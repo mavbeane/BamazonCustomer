2981104efe04fe6758e03dc0e3c2f7a92964dc54
Maria Beane <mbeane@findyr.com>
2016-10-26 19:00:53 -0500

    Completed Part 1

--- BamazonCustomer.js
+++ BamazonCustomer.js
@@ -15,20 +15,19 @@ connection.connect(function(err) {
 	console.log('connected as id ' + connection.threadId);
 });
 
-connection.query('SELECT * FROM Products', function(err, results) {
+function buy() {
+	connection.query('SELECT * FROM Products', function(err, results) {
 
-	if(err) {
-		console.log(err);
-	} else {
-		console.log('------------------------------------------------------');
-		console.log(' | ID  /  Product Name   /  Product Name  /  Price  | ');
-
-		for (var i=0; i < results.length; i++) {
-
-			console.log(' | ' + results[i].ItemID + ' / ' + results[i].ProductName + ' / ' + results[i].DepartmentName + ' / ' + results[i].Price + ' | ');
-
+		if(err) {
+			console.log(err);
+		} else {
+			console.log('------------------------------------------------------');
+			console.log('ID\tDEPARTMENT\tPRODUCT\t\tPRICE ');
+			for (var i=0; i < results.length; i++) {
+				console.log(+ results[i].ItemID + '\t' + results[i].DepartmentName + '\t\t' + results[i].ProductName + '\t$' + results[i].Price.toFixed(2));
+			};
+			console.log('------------------------------------------------------');	
 		};
-		console.log('------------------------------------------------------');
 
 		prompt.start();
 
@@ -41,27 +40,35 @@ connection.query('SELECT * FROM Products', function(err, results) {
 					description: ('How many units would you like to buy')
 				}
 			}
-			}, function (err, result) {
-		 
-			    console.log('Command-line input received:');
-			    console.log('  Product ID: ' + result.productID);
-			    console.log('  Units Wanted: ' + result.units);
+			}, function (err, res) {
+			   	
+			   	connection.query('SELECT * FROM Products WHERE ?', {ItemID: res.productID}, function(err, prodResults) {
+				   	
+					if (err) {
+						console.log(err);
+					};
 
-			    connection.query('SELECT * FROM Products WHERE ?', {ItemID: result.productID}, function(err, results) {
+					if (prodResults[0].StockQuantity < res.units) {
+						console.log('------------------------------------------------------');
+						console.log('------------------------------------------------------');
+						console.log('INSUFFICIENT QUANTITY! Please try another product.');
+						console.log('------------------------------------------------------');
+						buy();
+					} else {
+						connection.query('UPDATE Products SET ? WHERE ?', [{
+							StockQuantity: prodResults[0].StockQuantity - res.units
+						}, {
+							ItemID: res.productID
+						}], function(err, results) {
+							console.log('------------------------------------------------------');
+						});
+						console.log('Thank you for your purchase! \nProduct purchased: ' + prodResults[0].ProductName + '\nUnits purchased: ' + res.units);
+						var totalCharged = prodResults[0].Price * res.units
+						console.log('Total charged: $' + totalCharged.toFixed(2));
+					};
 
-				if(err) {
-					console.log(err);
-				} else {
-					//console.log('lllll' + result.productID);
-				};
-
-
-
-			  });
+			  	});
 			});
-
-	};
-
-	connection.end();
-});
-
+	});
+};
+buy();
